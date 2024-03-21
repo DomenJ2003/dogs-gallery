190930f63aa65821748bc54016e1d882d8f1fb43
DomenJakopic <domen.jakopic2@gmail.com>
2024-03-21 17:16:56 +0100

    links in app bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -6,9 +6,13 @@ const dogStore = useDogStore();
 </script>
 
 <template>  
-    <div class="app-bar" style="background-color: grey;">
+    <div class="app-bar">
         <h1 v-if="!dogStore.selectedBreed">Welcome to Dog Libery</h1>
         <h1 v-else>{{ dogStore.selectedBreed.toLocaleUpperCase() }}</h1>
+        <div class="links">
+            <a class="link" href="/">Home</a>
+            <a class="link" href="/favorites">Favorites</a>
+        </div>
     </div>
 </template>
 
@@ -18,9 +22,19 @@ const dogStore = useDogStore();
     top: 0;
     width: 100%;
     height: 80px;
+    background-color: #ccc;
     color: #888;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
+}
+
+.links{
+    margin-right: 50px;
+    gap: 30px;
+}
+
+.link{
+    padding: 5px;
 }
 </style>
